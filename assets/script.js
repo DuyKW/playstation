0d47158ff651373bab33ce77abc9177a6838cb8a
DUY NGUYEN KHAC <duy.nguyenkhacvnu@hcmut.edu.vn>
2024-04-04 12:22:10 +0700

    first commit

---
+++ assets/script.js
@@ -0,0 +1,128 @@
+
+const list2 = document.querySelector('.slider2 .slider-content');
+const items2 = document.querySelectorAll('.slider2 .slider-content .item');
+const dots_img = document.querySelectorAll('.slider2-contain .dots-img li');
+const next2 = document.getElementById('next2');
+const prev2 = document.getElementById('prev2');
+const list_img = document.querySelector('.slider2-contain .slider-control .dots-img')
+const img =document.querySelectorAll('.slider2-contain .dots-img img')
+const list = document.querySelector('.slider .list');
+const items = document.querySelectorAll('.slider .list .item');
+const dots = document.querySelectorAll('.slider .dots li');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const slider = document.querySelector('.slider');
+const btn_active = document.querySelectorAll('.grid-container .buttons button')
+let active = 0;
+const lengthItems = items.length - 1;
+
+let active2 = 0;
+const size2 = items2.length - 1;
+
+
+//-------------------------------------------GRID CONTAINER--------------------------------------
+let left = document.getElementById('left');
+let right = document.getElementById('right');
+let left_content = document.querySelectorAll('.grid-container .grid-content div');
+let right_content = document.querySelectorAll('.grid-container .grid-content-copy div');
+
+//Function to set height.
+let heightSlider = items[active].offsetHeight;
+slider.style.height = (heightSlider + 120) + 'px';
+
+
+next.addEventListener('click', function() {
+    active = (active + 1 > lengthItems) ? 0 : (active + 1);
+    reloadSlider();
+});
+
+prev.addEventListener('click', function() {
+    active = (active - 1 < 0) ? lengthItems : (active - 1);
+    reloadSlider();
+});
+
+let refreshSlider = setInterval (()=> {
+    next.click()},3000);
+function reloadSlider() {
+    const checkLeft = items[active].offsetLeft;
+    list.style.left = -checkLeft + 'px';
+    let lastActiveDot = document.querySelector('.slider .dots li.active');
+    lastActiveDot.classList.remove('active');
+    dots[active].classList.add('active');
+    clearInterval(refreshSlider);
+    refreshSlider = setInterval (()=> {
+        next.click()},3000);
+}
+dots.forEach((li,key) => {
+    li.addEventListener('click',function() {
+        active = key;
+        reloadSlider();
+    })
+})
+
+// ------------------------------------------------------------BEGIN-------------------------------------------------
+
+
+
+next2.addEventListener('click', function () {
+    if (active2 < size2) {
+        active2++;
+    }
+    refreshSlider2();
+});
+prev2.addEventListener('click', function () {
+    if (active2 > 0) {
+        active2--;
+    }
+    refreshSlider2();
+});
+function refreshSlider2() {
+    const checkLeft2 = items2[active2].offsetLeft;
+    list2.style.left = -checkLeft2 + 'px';
+
+    let lastActiveDot2 = document.querySelector('.slider2-contain .dots-img li.active');
+    lastActiveDot2.classList.remove('active');
+    dots_img[active2].classList.add('active');
+    if (active2 <= 4 ) {
+        let moveLeft = img[active].clientWidth;
+        list_img.style.left = -moveLeft*active2 + 'px';
+    }
+    
+}
+dots_img.forEach ((li,key)=> { 
+    li.addEventListener('click',function() {
+        active2 = key;
+        refreshSlider2();
+    })
+}) 
+
+left.addEventListener('click', function () {
+    const activeButton = document.querySelector('.grid-container .buttons button.active');
+    if (activeButton) { 
+      activeButton.classList.remove('active');
+      left.classList.add('active'); 
+        displaySet(0);
+    }
+});
+right.addEventListener('click', function () {
+    const activeButton = document.querySelector('.grid-container .buttons button.active');
+    if (activeButton) {
+      activeButton.classList.remove('active');
+      right.classList.add('active'); 
+        displaySet(1);
+    }  
+  });
+function displaySet (i) {
+    if (i === 0) {
+        for (let j = 0; j < right_content.length;j++) {
+        right_content[j].style.display = "none";
+        left_content[j].style.display = "unset";
+        }
+    }
+    else {
+        for(let j = 0; j < left_content.length;j++) {
+        right_content[j].style.display = "unset";
+        left_content[j].style.display = "none";
+        }
+    }
+}
